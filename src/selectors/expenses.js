8869f399fcf879321cab7d6904d19dce014d206b
Saabbir Hossain <thesaabbir@gmail.com>
2021-03-22 14:43:13 +0600

    Refactor get visible expenses selector

--- src/selectors/expenses.js
+++ src/selectors/expenses.js
@@ -2,13 +2,22 @@ import moment from 'moment';
 
 export default (expenses, filters) => {
   return expenses.filter(expense => {
-    const startDateMatch = filters.startDate ? filters.startDate.isSameOrBefore(expense.createdAt) : true;
-    const endDateMatch = filters.endDate ? filters.endDate.isSameOrAfter(expense.createdAt) : true;
-    const textMatch =
-      expense.description.toLowerCase().includes(filters.text.toLowerCase()) || expense.note.toLowerCase().includes(filters.text.toLowerCase());
 
+    // Create a moment object for expense creation date
+    const createdAtMoment = moment(expense.createdAt);
+
+    // Match start date filter
+    const startDateMatch = filters.startDate ? filters.startDate.isSameOrBefore(createdAtMoment, 'day') : true;
+
+    // Match end date filter
+    const endDateMatch = filters.endDate ? filters.endDate.isSameOrAfter(createdAtMoment, 'day') : true;
+
+    // Match search text
+    const textMatch = expense.description.toLowerCase().includes(filters.text.toLowerCase()) || expense.note.toLowerCase().includes(filters.text.toLowerCase());
+
+    // Return true if all cases matches
     return startDateMatch && endDateMatch && textMatch;
-  }).sort((a, b) => {
+  }).sort((a, b) => { // Chaining sort method to sort the filtered array
     if (filters.sortBy === 'date') {
       return a.createdAt < b.createdAt ? 1 : -1;
     } else if (filters.sortBy === 'amount') {
