e194dd470c8fb1a9c211835755d23a79fc619a1b
Saabbir Hossain <thesaabbir@gmail.com>
2021-03-24 13:05:10 +0600

    Format createdAt and amount using moment and numeral js

--- src/components/ExpenseListItem.js
+++ src/components/ExpenseListItem.js
@@ -1,14 +1,16 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
+import moment from 'moment';
+import numeral from 'numeral';
 
 const ExpenseListItem = ({ description, amount, createdAt, id, dispatch }) => (
   <div>
     <Link to={`/edit/${id}`}>
       <h2>{ description }</h2>
     </Link>
-    <p>Amount - { amount }</p>
-    <p>CreatedAt - { createdAt }</p>
+    <p>Amount - { numeral(amount / 100).format('$0,0.00') }</p>
+    <p>CreatedAt - { moment(createdAt).format('MMMM Do, YYYY') }</p>
   </div>
 );
 
