75c7f346ffa0c3141873dabcf4234099c510190c
Saabbir Hossain <thesaabbir@gmail.com>
2021-03-24 10:59:08 +0600

    Setup production webpack build

--- webpack.config.js
+++ webpack.config.js
@@ -1,35 +1,55 @@
 const path = require('path');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
-module.exports = {
-  entry: './src/index.js',
-  output: {
-    filename: 'bundle.js',
-    path: path.resolve(__dirname, 'public/scripts')
-  },
-  module: {
-    rules: [{
-      test: /\.js$/,
-      exclude: /node_modules/,
-      loader: 'babel-loader'
+module.exports = (env) => {
+  const isProduction = env.production;
+  
+  return {
+    entry: './src/index.js',
+    output: {
+      filename: 'bundle.js',
+      path: path.resolve(__dirname, 'public/dist')
     },
-    {
-      test: /\.s?css$/,
-      use: [
-        // Creates `style` nodes from JS strings
-        "style-loader",
-        // Translates CSS into CommonJS
-        "css-loader",
-        // Compiles Sass into CSS
-        "sass-loader",
-      ],
-    }
-  ]
-  },
-  mode: 'development',
-  devtool: 'eval-cheap-module-source-map',
-  devServer: {
-    contentBase: path.resolve(__dirname, 'public'),
-    publicPath: '/scripts/',
-    historyApiFallback: true
+    module: {
+      rules: [{
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader'
+      },
+      {
+        test: /\.s?css$/,
+        use: [
+          // isProduction ? 
+          // MiniCssExtractPlugin.loader : 
+          // 'style-loader',
+          MiniCssExtractPlugin.loader,
+          {
+            loader: 'css-loader',
+            options: {
+              sourceMap: true
+            }
+          },
+          {
+            loader: 'sass-loader',
+            options: {
+              sourceMap: true
+            }
+          },
+        ],
+      }
+    ]
+    },
+    mode: isProduction ? 'production' : 'development',
+    devtool: isProduction ? 'source-map' : 'inline-source-map',
+    devServer: {
+      contentBase: path.resolve(__dirname, 'public'),
+      publicPath: '/dist/',
+      historyApiFallback: true
+    },
+    plugins: [
+      new MiniCssExtractPlugin({
+        filename: 'bundle.css'
+      })
+    ],
   }
-}
+};
