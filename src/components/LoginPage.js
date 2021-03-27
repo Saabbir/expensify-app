44b78733bea6c7eb2bc307022c88b9da8a86341d
Saabbir Hossain <thesaabbir@gmail.com>
2021-03-27 22:00:39 +0600

    Style login page

--- src/components/LoginPage.js
+++ src/components/LoginPage.js
@@ -3,8 +3,14 @@ import { connect } from 'react-redux';
 import { startLogin } from '../actions/auth';
 
 const LoginPage = ({ startLogin }) => (
-  <div>
-    <button onClick={startLogin}>Login</button>
+  <div className="c-login-page">
+    <div className="c-login-page__content">
+      <h1 className="c-login-page__title">Expensify App</h1>
+      <p className="c-login-page__tagline">It's time to get your expenses under control.</p>
+      <div className="u-mt-32">
+        <button className="c-button" onClick={startLogin}>Log In</button>
+      </div>
+    </div>
   </div>
 );
 
