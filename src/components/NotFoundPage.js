43dbd575ff89c39353bb3bcc12282542298ff9fa
Saabbir Hossain <thesaabbir@gmail.com>
2021-04-10 21:32:32 +0600

    Style not found page

--- src/components/NotFoundPage.js
+++ src/components/NotFoundPage.js
@@ -2,8 +2,11 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 const NotFoundPage = () => (
-  <div>
-    <p>Page Not Found! - <Link to="/">Go Home</Link></p>
+  <div className="c-page-not-found">
+    <div className="l-wrap">
+      <h1 className="c-page-not-found__title">404!</h1>
+      <p>Page Not Found. <Link to="/">Go Home</Link>.</p>
+    </div>
   </div>
 );
 
